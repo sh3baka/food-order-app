5c1e931c3e4fd9c63c8d58960cbbedbc16b34695
k.b <napsterlv@gmail.com>
2021-09-19 20:23:44 +0300

    backdrop and header changes

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import mealsImage from '../../assets/meals.jpg'
 import classes from "./Header.module.css"
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
@@ -8,6 +9,7 @@ const Header = (props) => {
       <header className={classes.header}>
         <h1>ReactMeals</h1>
         <button>Cart</button>
+        <HeaderCartButton onClick={props.onShowCart}/>
       </header>
       <div className={classes["main-image"]}>
           <img src={mealsImage} alt="Table of food!"/>
