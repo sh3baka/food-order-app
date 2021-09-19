e3e1a881d39dcb44e9e527d5fee3fe0f581e2ccd
k.b <napsterlv@gmail.com>
2021-09-20 02:41:30 +0300

    done

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,43 @@
+import { useContext, useEffect, useState } from "react";
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
-import classes from './HeaderCartButton.module.css';
+import classes from "./HeaderCartButton.module.css";
 
-const HeaderCartButton = props => {
-return <button className={classes.button} onClick={props.onClick} >
-    <span className={classes.icon}>
+const HeaderCartButton = (props) => {
+  const [btnIsHighLighted, setBtnIsHighLighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  let btnClasses = `${classes.button} ${btnIsHighLighted ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighLighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighLighted(false);
+    }, 300);
+
+    return () => {
+        clearTimeout(timer)
+    }
+  }, [items]);
+
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
         <CartIcon />
-    </span>
-    <span>Your Cart</span>
-    <span className={classes.badge}>
-        3
-    </span>
-</button>
-}
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
 
 export default HeaderCartButton;
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,10 +1,13 @@
-import classes from './Input.module.css'
+import { forwardRef } from "react";
+import classes from "./Input.module.css";
 
-const Input = props => {
-    return <div className={classes.input}>
-        <label htmlFor={props.input.id}>{props.label}</label>
-        <input {...props.input} />
+const Input = forwardRef((props, ref) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input ref={ref} {...props.input} />
     </div>
-}
+  );
+});
 
-export default Input
+export default Input;
