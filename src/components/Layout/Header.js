b4a848a63b371285a5dd4286d121fb1a1e50c029
k.b <napsterlv@gmail.com>
2021-09-20 20:05:47 +0300

    fix

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,6 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
         <HeaderCartButton onClick={props.onShowCart}/>
       </header>
       <div className={classes["main-image"]}>
